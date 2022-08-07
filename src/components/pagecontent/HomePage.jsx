1e7924ce099b87baab621b571282975cbce35bdf
Saqib Hussain - Developer - UI / UX Designer <saqibhussaincg@gmail.com>
2022-08-07 11:49:39 -0700

    wrapper

--- src/components/pagecontent/HomePage.jsx
+++ src/components/pagecontent/HomePage.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import About from '../about/About'
 import Branding from '../home/Branding'
 import Home from '../homes/Home'
+import Service from '../servicess/Service'
+import Wrapper from '../Wrapper'
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
     <Home />
     <Branding />
     <About />
+    <Service />
+    <Wrapper />
     </>
   )
 }
