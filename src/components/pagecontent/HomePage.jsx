21d90f0135a6cb59d1224c6c1c9e2e5e2bef704c
Saqib Hussain - Developer - UI / UX Designer <saqibhussaincg@gmail.com>
2022-08-07 09:47:03 -0700

    about

--- src/components/pagecontent/HomePage.jsx
+++ src/components/pagecontent/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import About from '../about/About'
+import Branding from '../home/Branding'
 import Home from '../homes/Home'
 
 const HomePage = () => {
   return (
     <>
     <Home />
+    <Branding />
+    <About />
     </>
   )
 }
